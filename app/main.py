bd664184336a4ed75a319cec3d81f55983fbff9e
TheAshenArkhon <80908900+TheAshenArkhon@users.noreply.github.com>
2025-02-25 22:15:33 -0300

    permissoes CORS

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 # app/main.py
 from fastapi import FastAPI
 from app.routers import upload, history, search
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="Desafio Dev API",
@@ -10,6 +11,15 @@ app = FastAPI(
     redoc_url="/redoc",  # URL do Redoc
     openapi_url="/openapi.json"  # Especificação OpenAPI
     )
+
+# Configuração do CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permite chamadas de qualquer domínio (ajuste para produção)
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos os métodos (GET, POST, etc.)
+    allow_headers=["*"],  # Permite todos os headers
+)
 
 app.include_router(upload.router, prefix="/upload", tags=["Upload"])
 app.include_router(history.router, prefix="/history", tags=["Histórico"])
